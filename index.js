ae0429f527c285d77ca2e5dd6b62d15cc95d7365
Flatiron School <andrewsprague@outlook.com>
2018-10-09 09:13:11 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (arr) {
+    return arr.slice(0,2);
+};
+
+const returnLastTwoDrivers = function (arr) {
+    return arr.slice(-2);
+};
+
+const selectingDrivers = [
+    returnFirstTwoDrivers, returnLastTwoDrivers
+];
+
+const createFareMultiplier = function (x) {
+    return function multiplier(y) {
+        return y * x;
+    };
+};
+
+function fareDoubler(x) {
+    return x * 2;
+};
+
+const fareTripler = function (x) {
+    return x * 3;
+};
+
+const fetchSpecifiedDrivers = function (array, func) {
+    return func(array);
+};
+
